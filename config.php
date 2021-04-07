439ee5469dbd38e58e5f6393685125bd7d03ad69
vylen17 <58108404+vylen17@users.noreply.github.com>
2021-04-08 05:57:41 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,5 +4,5 @@
 $inputList = ''; // list.txt
 
 // telegram
-$telegramToken = ''; // token telegram bot
-$telegramChatId = ''; // chat id telegram
+$telegramToken = ''; // 1752027795:AAHGvg4eiXGmLtW_tXZBVTNlmII-owV-4Q0
+$telegramChatId = ''; // 1620613902
